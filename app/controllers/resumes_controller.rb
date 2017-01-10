1801d8a293973440096025e83de2aec6b6665f3e
Jacob Szemanski <jjsze13@gmail.com>
2017-01-09 20:38:41 -0600

    Added views for resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,15 @@
 class ResumesController < ApplicationController
 
   def index 
+    @student = Unirest.get "A P I", {student["email"]=> session[:email]}
+  end
+
+  def edit
+    @student = Unirest.get "A P I", {student["email"]=> session[:email]}
+  end
+
+  def update
+    
   end
 
 end
