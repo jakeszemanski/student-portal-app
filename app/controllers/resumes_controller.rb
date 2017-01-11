cd36ca9fd1407b573251eb65350422f5f692fdcf
Jacob Szemanski <jjsze13@gmail.com>
2017-01-10 19:01:43 -0600

    Added a experiences, skills, and education controller and edit views

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,22 +1,9 @@
 class ResumesController < ApplicationController
 
   def index 
-    @student = Unirest.get "A P I", {student["email"]=> session[:email]}
-  end
-
-  def edit
-    @student = Unirest.get("A P I", {student["email"]=> session[:email]}).body
-    @experiences = Unirest.get("A P I", {expereinces["student_id"]=> @student.id}).body
-    render "edit.html.erb"
-  end
-
-  def update
-    
-    # @student = Unirest.patch(
-    #   "API",
-    #   parameters: {
-    #     student[expereince][start_time]
-    #     }).body
+    @experiences = Unirest.get("A P I", experiences["student_id"]=> session[:id])
+    @educations = Unirest.get("A P I", educations["student_id"]=> session[:id])
+    @skills = Unirest.get("A P I", skills["student_id"]=> session[:id])
   end
 end
 
---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,10 @@
+class SkillsController < ApplicationController
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
