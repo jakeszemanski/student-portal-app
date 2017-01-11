496e496bb54568df4b3773782a1f09468090e57f
Jacob Szemanski <jjsze13@gmail.com>
2017-01-10 21:07:08 -0600

    Some url changes and updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/index' => 'resumes#index'
-  get '/capstones/:id' => 'capstones#show'
-  get '/capstones/:id/edit' => 'capstones#edit'
-  patch '/capstones/:id' => 'capstones#update'
+  get '/api/v1/resume' => 'resumes#index'
+  
+  get '/api/v1/experiences/:id' => 'experiences#show'
+  get '/api/v1/experiences/:id/edit' => 'experiences#edit'
+  patch '/api/v1/experiences/:id' => 'experiences#update'
+
+  get '/api/v1/educations/:id' => 'educations#show'
+  get '/api/v1/educations/:id/edit' => 'educations#edit'
+  patch '/api/v1/educations/:id' => 'educations#update'
+
+  get '/api/v1/skills/:id' => 'skills#show'
+  get '/api/v1/skills/:id/edit' => 'skills#edit'
+  patch '/api/v1/skills/:id' => 'skills#update'
+
+  get '/api/v1/capstones/:id' => 'capstones#show'
+  get '/api/v1/capstones/:id/edit' => 'capstones#edit'
+  patch '/api/v1/capstones/:id' => 'capstones#update'
 
 
 end
