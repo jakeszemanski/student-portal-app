448565d7f1737dfadc435c8c6e9ee02b8931ca82
Jennifer Lubejko <jennifer.lubejko@gmail.com>
2017-01-10 20:41:13 -0600

    Adding Educations controller and edit view

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,9 +1,21 @@
 class EducationsController < ApplicationController
-  def edit
 
+  def edit
+    @educations = Unirest.get("A P I", {educations["student_id"]=> session[:id]}).body
+    render 'edit.html.erb'
   end
 
   def update
-
+    @education = Unirest.patch(
+      "API",
+      parameters: {
+        start_date: params[:start_date],
+        end_date: params[:end_date],
+        degree: params[:degree],
+        university_name: params[:university_name],
+        details: params[:details]
+        }
+        ).body
+    redirect_to "/students/resume/#{session[:id]}"
   end
 end
