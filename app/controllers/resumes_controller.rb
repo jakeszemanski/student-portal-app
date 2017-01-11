c57105035ff889f468be43f729b94198c6c743f6
Jacob Szemanski <jjsze13@gmail.com>
2017-01-10 20:13:26 -0600

    Added coded to the resume show page

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,8 +1,11 @@
 class ResumesController < ApplicationController
   def show
-    @experiences = Unirest.get("A P I", experiences["student_id"]=> session[:id])
-    @educations = Unirest.get("A P I", educations["student_id"]=> session[:id])
-    @skills = Unirest.get("A P I", skills["student_id"]=> session[:id])
+
+    @experiences = Unirest.get("A P I", experiences["student_id"]=> session[:id]).body
+    @educations = Unirest.get("A P I", educations["student_id"]=> session[:id]).body
+    @skills = Unirest.get("A P I", skills["student_id"]=> session[:id]).body
+
+    render 'show.html.erb'
   end
 end
 
